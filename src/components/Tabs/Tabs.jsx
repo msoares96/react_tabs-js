431465dd82685268be0fe18c942f7e1b0f594573
Matheus Soares <matheus.soares.8d@gmail.com>
2025-04-15 16:07:45 -0300

    Attempt 1

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,32 @@
-export const Tabs = () => {};
+export function getTabById(tabs, id) {
+  return tabs.find(tab => tab.id === id || null);
+}
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={tab.id === activeTabId ? `is-active` : ``}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => onTabSelected(tab.id)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {getTabById(tabs, activeTabId).content}
+      </div>
+    </div>
+  );
+};
